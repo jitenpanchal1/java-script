14098598b6c01f0d269a3d9115d29688b522e38e
jitenpanchal1 <jeetenpanchal84@gmail.com>
2025-03-27 13:58:02 +0530

    data

---
+++ slik slider/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  // $(".single-item").slick();
+
+  $(".responsive").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<button class="slick-prev">&larr;</button>', // Custom left arrow
+    nextArrow: '<button class="slick-next">&rarr;</button>', // Custom right arrow
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
